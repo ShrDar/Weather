5a7a038e1f881a7fcc421d7d95af45346ff68d91
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2023-05-02 19:31:23 +0545

    Added: Name Changes

--- Weather/DarshanShrestha_2329504.js
+++ Weather/DarshanShrestha_2329504.js
@@ -16,25 +16,25 @@ if(time[2]=="d"){
     document.body.style.background="var(--daybg)";
     if(condition=="Clouds"){
         document.getElementById("status").innerHTML="Cloudy";
-        document.getElementById("weather-icon").src="Pictures/mostlycloudy.png";
+        document.getElementById("weather-icon").src="mostlycloudy.png";
         if(con1=="few clouds"){
             document.getElementById("status").innerHTML="Mostly Sunny";
-            document.getElementById("weather-icon").src="Pictures/mostlysunny.png";
+            document.getElementById("weather-icon").src="mostlysunny.png";
         }
         if(con1=="overcast clouds"){
             document.getElementById("status").innerHTML="Overcast Clouds";
-            document.getElementById("weather-icon").src="Pictures/cloudy.png";
+            document.getElementById("weather-icon").src="cloudy.png";
             document.getElementById("container").style.background="var(--overcastcontainerbg)";
             document.body.style.background="var(--overcastbg)";
         }
     }
     else if(condition=="Clear"){
         document.getElementById("status").innerHTML="Sunny";
-        document.getElementById("weather-icon").src="Pictures/sunny.png";
+        document.getElementById("weather-icon").src="sunny.png";
     }
     else if(condition=="Haze"){
         document.getElementById("status").innerHTML="Hazy";
-        document.getElementById("weather-icon").src="Pictures/haze.png";
+        document.getElementById("weather-icon").src="haze.png";
         document.getElementById("container").style.background="var(--hazycontainerbg)";
         document.body.style.background="var(--hazybg)";
 
@@ -46,30 +46,30 @@ if(time[2]=="d"){
         document.body.style.background="var(--snowybg)";
 
         document.getElementById("status").innerHTML="Snowy";
-        document.getElementById("weather-icon").src="Pictures/snowy.png";
+        document.getElementById("weather-icon").src="snowy.png";
     }
     else if(condition=="Rain"){
         document.getElementById("status").innerHTML="Rainy";
-        document.getElementById("weather-icon").src="Pictures/rainy.png";
+        document.getElementById("weather-icon").src="rainy.png";
     }
     else if(condition=="Fog"){
         document.getElementById("status").innerHTML="Foggy";
-        document.getElementById("weather-icon").src="Pictures/foggy.png";
+        document.getElementById("weather-icon").src="foggy.png";
     }
     else if(condition=="Smoke"){
         document.getElementById("container").style.background="var(--smokycontainerbg)";
         document.getElementById("status").innerHTML="Smoky";
         document.getElementById("container").style.color="#fff";
-        document.getElementById("weather-icon").src="Pictures/smoky.png";
+        document.getElementById("weather-icon").src="smoky.png";
         document.body.style.background="var(--smokybg)";
     }
     else if(condition=="Drizzle"){
         document.getElementById("status").innerHTML="Drizzly";
-        document.getElementById("weather-icon").src="Pictures/drizzly.png";
+        document.getElementById("weather-icon").src="drizzly.png";
     }
     else if(condition=="Mist"){
         document.getElementById("status").innerHTML="Misty";
-        document.getElementById("weather-icon").src="Pictures/drizzly.png";
+        document.getElementById("weather-icon").src="drizzly.png";
     }
     else{
         document.getElementById("status").innerHTML=condition;
@@ -83,54 +83,54 @@ else if(time[2]=="n"){
     document.body.style.background="var(--nightbg)"
     if(condition=="Clouds"){
         document.getElementById("status").innerHTML="Cloudy";
-        document.getElementById("weather-icon").src="Pictures/mostlycloudymoon.png";
+        document.getElementById("weather-icon").src="mostlycloudymoon.png";
         if(con1=="few clouds"){
             document.getElementById("status").innerHTML="Few Clouds";
-            document.getElementById("weather-icon").src="Pictures/fewcloudsnight.png";
+            document.getElementById("weather-icon").src="fewcloudsnight.png";
         }
         if(con1=="overcast clouds"){
             document.getElementById("status").innerHTML="Overcast Clouds";
-            document.getElementById("weather-icon").src="Pictures/cloudy.png";
+            document.getElementById("weather-icon").src="cloudy.png";
         }
     }
     else if(condition=="Clear"){
         document.getElementById("status").innerHTML="Clear Night";
-        document.getElementById("weather-icon").src="Pictures/moon.png";
+        document.getElementById("weather-icon").src="moon.png";
     }
     else if(condition=="Haze"){
         document.getElementById("status").innerHTML="Hazy";
-        document.getElementById("weather-icon").src="Pictures/hazenight.png";
+        document.getElementById("weather-icon").src="hazenight.png";
     }
     else if(condition=="Snow"){
         document.getElementById("status").innerHTML="Snowy";
-        document.getElementById("weather-icon").src="Pictures/snowy.png";
+        document.getElementById("weather-icon").src="snowy.png";
     }
     else if(condition=="Rain"){
         document.getElementById("status").innerHTML="Rainy";
-        document.getElementById("weather-icon").src="Pictures/rainy.png";
+        document.getElementById("weather-icon").src="rainy.png";
     }
     else if(condition=="Fog"){
         document.getElementById("status").innerHTML="Foggy";
-        document.getElementById("weather-icon").src="Pictures/foggy.png";
+        document.getElementById("weather-icon").src="foggy.png";
     }
     else if(condition=="Thunderstorm"){
         document.getElementById("status").innerHTML="Thunderstorm";
-        document.getElementById("weather-icon").src="Pictures/thunderstorm.png";
+        document.getElementById("weather-icon").src="thunderstorm.png";
     }
     else if(condition=="Smoke"){
         document.getElementById("container").style.background="var(--smokycontainerbg)";
         document.getElementById("status").innerHTML="Smoky";
         document.getElementById("container").style.color="#fff";
-        document.getElementById("weather-icon").src="Pictures/smoky.png";
+        document.getElementById("weather-icon").src="smoky.png";
         document.body.style.background="var(--smokybg)";
     }
     else if(condition=="Drizzle"){
         document.getElementById("status").innerHTML="Drizzly";
-        document.getElementById("weather-icon").src="Pictures/drizzly.png";
+        document.getElementById("weather-icon").src="drizzly.png";
     }
     else if(condition=="Mist"){
         document.getElementById("status").innerHTML="Misty";
-        document.getElementById("weather-icon").src="Pictures/misty.png";
+        document.getElementById("weather-icon").src="misty.png";
     }
     else{
         document.getElementById("status").innerHTML=condition;
@@ -159,7 +159,7 @@ function fetchweather(query){
         document.getElementById("city").innerHTML="Not Found";
         document.getElementById("temp").innerHTML="-";
         document.getElementById("status").innerHTML="---";
-        document.getElementById("weather-icon").src="Pictures/notfound.png";
+        document.getElementById("weather-icon").src="notfound.png";
         document.getElementById("rainp").innerHTML="--";
         document.getElementById("humidp").innerHTML="--";
         document.getElementById("windp").innerHTML="--";
